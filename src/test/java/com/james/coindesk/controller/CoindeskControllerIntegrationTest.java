c4dccf3e35c4bbb577b330a79a3c01f2fcc623b0
james <mary0520kk@gmail.com>
2023-05-29 11:00:47 +0800

    fix: set spring-boot-admin

--- src/test/java/com/james/coindesk/controller/CoindeskControllerIntegrationTest.java
+++ src/test/java/com/james/coindesk/controller/CoindeskControllerIntegrationTest.java
@@ -9,15 +9,19 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.server.LocalServerPort;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
-@SpringBootTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @AutoConfigureMockMvc
 @TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 class CoindeskControllerIntegrationTest {
+    @LocalServerPort
+    protected int port;
+
     @Autowired
     private CoindeskClientService coindeskClientService;
 
--- src/test/java/com/james/coindesk/controller/CurrencyNameControllerIntegrationTest.java
+++ src/test/java/com/james/coindesk/controller/CurrencyNameControllerIntegrationTest.java
@@ -11,16 +11,19 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.server.LocalServerPort;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
-@SpringBootTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @AutoConfigureMockMvc
 @TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 class CurrencyNameControllerIntegrationTest {
+    @LocalServerPort
+    protected int port;
 
     @Autowired
     private CurrencyNameService currencyNameService;
